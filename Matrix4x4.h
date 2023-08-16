d9ba0786e1f89c70d43081d1d044fc9aca70a73d
Luc Edes <60040621+lucedes27@users.noreply.github.com>
2023-08-16 11:38:33 +0200

    Create Matrix4x4.h

---
+++ Matrix4x4.h
@@ -0,0 +1,39 @@
+//
+//
+//
+
+#ifndef RAY_TRACER_MATRIX4X4_H
+#define RAY_TRACER_MATRIX4X4_H
+
+
+class Matrix4x4 {
+public:
+    float m[4][4];
+
+    // Constructor initializes to identity matrix
+    Matrix4x4() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                m[i][j] = (i == j) ? 1.0f : 0.0f;
+            }
+        }
+    }
+
+    // Matrix multiplication
+    Matrix4x4 operator*(const Matrix4x4& other) const {
+        Matrix4x4 result;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                result.m[i][j] = 0;
+                for (int k = 0; k < 4; k++) {
+                    result.m[i][j] += m[i][k] * other.m[k][j];
+                }
+            }
+        }
+        return result;
+    }
+    
+};
+
+
+#endif //RAY_TRACER_MATRIX4X4_H
