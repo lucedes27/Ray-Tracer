379f962c42a0e4a3422122637820c6a41d3c8d21
Luc Edes <60040621+lucedes27@users.noreply.github.com>
2023-08-17 19:18:31 +0200

    Fix Light constructor bug

--- Light.h
+++ Light.h
@@ -15,11 +15,11 @@ public:
 
     Vector3 direction; // Normalized direction of the light (for directional)
     Vector3 position;  // Position of the light (for point)
-    Vector3 color; // Color intensity
+    Vector3 color;     // Color intensity
     Type type;
 
     Light(Type type, const Vector3& directionOrPosition, const Vector3& color)
-            : direction(directionOrPosition), color(color), type(type) {
+            : color(color), type(type) {
         if (type == Type::Directional) {
             direction = directionOrPosition.normalize();
         } else {
